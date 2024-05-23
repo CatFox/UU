21848f2f3b78846f1c8c9105aaf902ab99d4e3e3
Stanislav <CatFox@live.ru>
2024-05-23 10:31:33 +0300

    initial code

---
+++ module2/add_homework3.py
@@ -0,0 +1,20 @@
+import random
+
+
+def make_list():
+    list_ = []
+    for i in range(1, 10001):
+        list_.append(random.randint(1, 10000))
+    return list_
+
+
+main_list = make_list()
+average = sum(main_list) / len(main_list)
+print("Среднее арифметическое:", average)
+main_list.sort(reverse=True)
+for number in main_list:
+    if number > average:
+        if number % 5 == 0:
+            print(number ** 0.5)
+    else:
+        break
